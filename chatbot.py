84821d0a6442a5379427014006b99586113b8fbd
Nostra18 <seraphinbaributsa@gmail.com>
2024-08-13 20:36:25 +0200

    user-friendly answers

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 import os 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.agent_toolkits import create_sql_agent
+from langchain_core.prompts import ChatPromptTemplate
 
 # Read the csv file
 # df = pd.read_csv("data/df.csv")
@@ -23,11 +24,17 @@ llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 # LangChain Agent
 agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
 
-# query = "number of disaster in jun 2010"
-
-# response = agent_executor.invoke({"input": query})
-
-# print(f"--> {response['output']}")
+# Context + Template 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are a helpful assistant about disasters. If you don't know a response don't say it. 
+            And tell the user how you can help him but if it's a greeting greet the user """,
+        ),
+        ("human", "{input}"),
+    ]
+)
 
 # Streamlit app
 import streamlit as st 
@@ -43,17 +50,18 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # React to user input
-if prompt := st.chat_input("This bot help you to find informations about disasters ?"):
+if user_input := st.chat_input("This bot help you to find informations about disasters ?"):
     # Display user message in chat message container
     with st.chat_message("user"):
-        st.markdown(prompt)
+        st.markdown(user_input)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    query = prompt
+    query = user_input
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = agent_executor.invoke({"input": query})['output']
+        chain = prompt | agent_executor 
+        response = chain.invoke({"input": query})['output']
         st.write(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
